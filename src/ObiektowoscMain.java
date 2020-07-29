8f8db98d88eb17e0fc1036b01797f337acc91918
Szymon Leyk <leyas@wp.pl>
2020-07-29 14:19:24 +0200

    #3 formatowanie kodu

--- src/ObiektowoscMain.java
+++ src/ObiektowoscMain.java
@@ -14,23 +14,23 @@ public class ObiektowoscMain {
         Samochod sluzbowy = new Samochod();
         sluzbowy.marka = "Opel";
         sluzbowy.rokProdukcji = 2000;
-        sluzbowy.cena= 100000;
+        sluzbowy.cena = 100000;
 
 
         Samochod zony = prywatny;
 
-       zony.info();
-       prywatny.info();
-       sluzbowy.info();
+        zony.info();
+        prywatny.info();
+        sluzbowy.info();
 
         zony.kolor = "zielony";
 
-        System.out.println("zony, marka: "+zony.marka);
-        System.out.println("zony, kolor: "+zony.kolor);
-        System.out.println("prywatny, marka: "+prywatny.marka);
-        System.out.println("prywatny, kolor: "+prywatny.kolor);
-        System.out.println("sluzbowy, marka: "+sluzbowy.marka);
-        System.out.println("sluzbowy, kolor: "+sluzbowy.kolor);
+        System.out.println("zony, marka: " + zony.marka);
+        System.out.println("zony, kolor: " + zony.kolor);
+        System.out.println("prywatny, marka: " + prywatny.marka);
+        System.out.println("prywatny, kolor: " + prywatny.kolor);
+        System.out.println("sluzbowy, marka: " + sluzbowy.marka);
+        System.out.println("sluzbowy, kolor: " + sluzbowy.kolor);
 
 
         /******* wywołanie metody ****/
@@ -42,14 +42,14 @@ public class ObiektowoscMain {
 
         boolean czyPrywatnyJestZabytkiem = prywatny.czyJestZabytkiem();
         boolean czySluzbowyJestZabytkiem = sluzbowy.czyJestZabytkiem();
-        System.out.println("czyPrywatnyJestZabytkiem: "+czyPrywatnyJestZabytkiem);
-        System.out.println("czySluzbowyJestZabytkiem: "+czySluzbowyJestZabytkiem);
+        System.out.println("czyPrywatnyJestZabytkiem: " + czyPrywatnyJestZabytkiem);
+        System.out.println("czySluzbowyJestZabytkiem: " + czySluzbowyJestZabytkiem);
 
-        System.out.println("Jaka jest cena auta? "+prywatny.cena);
-        System.out.println("Jaka jest cena po rabacie 20%? "+prywatny.cenaPoRabacie(0.2));
+        System.out.println("Jaka jest cena auta? " + prywatny.cena);
+        System.out.println("Jaka jest cena po rabacie 20%? " + prywatny.cenaPoRabacie(0.2));
 
-        System.out.println("Jaka jest cena auta? "+sluzbowy.cena);
-        System.out.println("Jaka jest cena po rabacie 90%? "+sluzbowy.cenaPoRabacie(0.9));
+        System.out.println("Jaka jest cena auta? " + sluzbowy.cena);
+        System.out.println("Jaka jest cena po rabacie 90%? " + sluzbowy.cenaPoRabacie(0.9));
 
     }
 }
