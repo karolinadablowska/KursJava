c9f9aaf609b7bbf3d80fafd62f9fc63b0926ae21
Szymon Leyk <leyas@wp.pl>
2020-07-29 14:17:52 +0200

    #3 zdefiniowane metod, różne rodzaje metod, wywołanie metod

--- src/ObiektowoscMain.java
+++ src/ObiektowoscMain.java
@@ -8,23 +8,20 @@ public class ObiektowoscMain {
         prywatny.marka = "Fiat";
         prywatny.rokProdukcji = 1999;
         prywatny.kolor = "czerwony";
+        prywatny.cena = 2222;
 
         // deklara i alokacja
         Samochod sluzbowy = new Samochod();
         sluzbowy.marka = "Opel";
+        sluzbowy.rokProdukcji = 2000;
+        sluzbowy.cena= 100000;
 
 
         Samochod zony = prywatny;
 
-        System.out.println("zony, marka: "+zony.marka);
-        System.out.println("zony, kolor: "+zony.kolor);
-        System.out.println("zony, rok produkcji: "+zony.rokProdukcji);
-        System.out.println("prywatny, marka: "+prywatny.marka);
-        System.out.println("prywatny, kolor: "+prywatny.kolor);
-        System.out.println("prywatny, rok produkcji: "+prywatny.rokProdukcji);
-        System.out.println("sluzbowy, marka: "+sluzbowy.marka);
-        System.out.println("sluzbowy, kolor: "+sluzbowy.kolor);
-        System.out.println("sluzbowy, rok produkcji: "+sluzbowy.rokProdukcji);
+       zony.info();
+       prywatny.info();
+       sluzbowy.info();
 
         zony.kolor = "zielony";
 
@@ -34,6 +31,25 @@ public class ObiektowoscMain {
         System.out.println("prywatny, kolor: "+prywatny.kolor);
         System.out.println("sluzbowy, marka: "+sluzbowy.marka);
         System.out.println("sluzbowy, kolor: "+sluzbowy.kolor);
+
+
+        /******* wywołanie metody ****/
+        // metoda bez argumentów, nic nie zwracająca
+        prywatny.uruchom();
+        sluzbowy.uruchom();
+
+        prywatny.rokProdukcji = 1900;
+
+        boolean czyPrywatnyJestZabytkiem = prywatny.czyJestZabytkiem();
+        boolean czySluzbowyJestZabytkiem = sluzbowy.czyJestZabytkiem();
+        System.out.println("czyPrywatnyJestZabytkiem: "+czyPrywatnyJestZabytkiem);
+        System.out.println("czySluzbowyJestZabytkiem: "+czySluzbowyJestZabytkiem);
+
+        System.out.println("Jaka jest cena auta? "+prywatny.cena);
+        System.out.println("Jaka jest cena po rabacie 20%? "+prywatny.cenaPoRabacie(0.2));
+
+        System.out.println("Jaka jest cena auta? "+sluzbowy.cena);
+        System.out.println("Jaka jest cena po rabacie 90%? "+sluzbowy.cenaPoRabacie(0.9));
 
     }
 }
