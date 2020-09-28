caca056c34f2e776553d41aea6e63ee41d99cea1
Szymon Leyk <leyas@wp.pl>
2020-09-28 11:32:27 +0200

    #28 użycie biblioteki zewnętrznej, konfiguracja ręczna w intellij, na przykładzie gson.jar

---
+++ src/pl/karolina/java/gson/Osoba.java
@@ -0,0 +1,11 @@
+package pl.karolina.java.gson;
+
+public class Osoba {
+    private String imie;
+    private int wiek;
+
+    public Osoba(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+}
