13655a22d5ca4085699cc6466c9bd02afe8035e0
Szymon Leyk <leyas@wp.pl>
2020-10-05 11:19:21 +0200

    #28 generowanie osób w pętli

--- src/main/java/pl/karolina/java/gson/Main.java
+++ src/main/java/pl/karolina/java/gson/Main.java
@@ -1,21 +1,23 @@
 package pl.karolina.java.gson;
 
 import com.google.gson.Gson;
+import pl.szymonleyk.generateddata.GeneratePersonData;
 
 public class Main {
     public static void main(String[] args) {
         Gson gson = new Gson();
 
-        Osoba os1 = new Osoba("Marek", 45);
+        Osoba os1 = new Osoba(GeneratePersonData.generateName(), 45);
         String os1AsJson = gson.toJson(os1);
         System.out.println(os1AsJson);
 
-        Grupa grupa = new Grupa(5, "Grupa 1");
-        grupa.dodaj(new Osoba("Jurek", 22));
-        grupa.dodaj(new Osoba("Czarek", 25));
-        grupa.dodaj(new Osoba("Franek", 11));
-        grupa.dodaj(new Osoba("Kasia", 75));
-        grupa.dodaj(new Osoba("Ania", 66));
+        int sizeOfGroup = 100_000;
+
+        Grupa grupa = new Grupa(sizeOfGroup, "Grupa 1");
+        for(int i = 0; i < sizeOfGroup; i++){
+            grupa.dodaj(new Osoba(GeneratePersonData.generateName(), GeneratePersonData.generateAge()));
+        }
+
         os1AsJson = gson.toJson(grupa);
         System.out.println(os1AsJson);
     }
