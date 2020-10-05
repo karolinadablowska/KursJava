47c02c381c5f6359df9bd244ef99cee84c18b347
Szymon Leyk <leyas@wp.pl>
2020-10-05 11:55:04 +0200

    #32 test szukanie ksiazki w bibliotece

--- src/test/java/pl/karolina/BibliotekaTest.java
+++ src/test/java/pl/karolina/BibliotekaTest.java
@@ -1,5 +1,6 @@
 package pl.karolina;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BibliotekaTest {
 
     @Test
+    @DisplayName("Dodaj jedną książkę.")
     public void dodajJednaKsiazkeTest(){
         // given
         Biblioteka biblioteka = new Biblioteka();
@@ -19,4 +21,21 @@ public class BibliotekaTest {
         //then
         assertEquals(1,ileKsiazek);
     }
+
+    @Test
+    public void poszukajKsiazkeTest(){
+        // given
+        Ksiazka k1 = new Ksiazka("Kompendium programisty Java.", "Jurek Nowak");
+        Ksiazka k2 = new Ksiazka("SQL od podstaw.", "Marek");
+        Ksiazka[] ksiazki = {k1, k2};
+        Biblioteka biblioteka = new Biblioteka(ksiazki);
+
+        // when
+        Ksiazka ksiazka = biblioteka.poszukajKsiazke("SQL od podstaw.");
+
+        // then
+        assertEquals(ksiazka, k2);
+    }
+
+
 }
