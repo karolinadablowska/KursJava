d534b3fceabf605725aa0e6a30b35da5213a2d98
Szymon Leyk <leyas@wp.pl>
2020-10-05 12:01:55 +0200

    #32 dodanie implemenacji meteody poszukaj ksiazke

--- src/main/java/pl/karolina/Biblioteka.java
+++ src/main/java/pl/karolina/Biblioteka.java
@@ -4,11 +4,26 @@ public class Biblioteka {
     private int iterator = 0;
     Ksiazka[] ksiazki = new Ksiazka[10];
 
+    public Biblioteka(){}
+
+    public Biblioteka(Ksiazka[] ksiazki) {
+        this.ksiazki = ksiazki;
+    }
+
     public void dodajKsiazke(Ksiazka ksiazka) {
         ksiazki[iterator++] = ksiazka;
     }
 
     public int ileKsiazek() {
         return iterator;
+    }
+
+    public Ksiazka poszukajKsiazke(String tytulKsiazki) {
+        for(int i = 0; i < ksiazki.length && ksiazki[i] != null; i++){
+            if(ksiazki[i].getTytul().equals(tytulKsiazki)){
+                return ksiazki[i];
+            }
+        }
+        return null;
     }
 }
--- src/main/java/pl/karolina/Ksiazka.java
+++ src/main/java/pl/karolina/Ksiazka.java
@@ -8,4 +8,8 @@ public class Ksiazka {
         this.tytul = tytul;
         this.autor = autor;
     }
+
+    public String getTytul() {
+        return tytul;
+    }
 }
