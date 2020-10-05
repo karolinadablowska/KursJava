568774fbe94dda42adcebf1789f98cdfc99684bb
Szymon Leyk <leyas@wp.pl>
2020-10-05 12:20:24 +0200

    #32 repeatedtest, parametrizedTest

--- src/test/java/pl/karolina/BibliotekaTest.java
+++ src/test/java/pl/karolina/BibliotekaTest.java
@@ -2,8 +2,11 @@ package pl.karolina;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BibliotekaTest {
 
@@ -36,6 +39,41 @@ public class BibliotekaTest {
         // then
         assertEquals(ksiazka, k2);
     }
+
+    @Test
+    public void poszukajKsiazkeKtoraNieIstniejeTest(){
+        // given
+        Ksiazka k1 = new Ksiazka("Kompendium programisty Java.", "Jurek Nowak");
+        Ksiazka k2 = new Ksiazka("SQL od podstaw.", "Marek");
+        Ksiazka[] ksiazki = {k1, k2};
+        Biblioteka biblioteka = new Biblioteka(ksiazki);
+
+        // when
+        Ksiazka ksiazka = biblioteka.poszukajKsiazke("Takiej ksiazki nie ma");
+
+        // then
+        assertNull(ksiazka);
+    }
+
+    @ParameterizedTest(name = "Ksiazka o nazwie {0}, nie istenieje")
+    @CsvSource({ "JKASdfjla", "ADKFA", "Nie istniejaca"	})
+    public void poszukajKsiazkeKtoraNieIstniejeTest(String tytulKsiazki){
+        // given
+        Ksiazka k1 = new Ksiazka("Kompendium programisty Java.", "Jurek Nowak");
+        Ksiazka k2 = new Ksiazka("SQL od podstaw.", "Marek");
+        Ksiazka[] ksiazki = {k1, k2};
+        Biblioteka biblioteka = new Biblioteka(ksiazki);
+
+        // when
+        Ksiazka ksiazka = biblioteka.poszukajKsiazke(tytulKsiazki);
+
+        // then
+        assertNull(ksiazka);
+    }
+
+
+
+
 
 
 }
